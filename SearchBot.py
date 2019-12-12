48418685fe82332d222ea3dc829bacfedc114ebc
Flamingwizard4 <flamingwizard4@gmail.com>
2019-12-12 16:56:49 -0500

    Add files via upload

--- SearchBot.py
+++ SearchBot.py
@@ -1,36 +1,170 @@
-##SearchBot e.g. SearchBot.py www.youtube.com or SearchBot.py engineering -r 5 -n 50
-#A program to facilitate research, meaning Google search + web scraping + content compilation + LP summarization
+## ResearchBot
+# A program to facilitate research, meaning Google search + web scraping + content compilation + LP summarization
 # TODO:
-# receive query from command line
-# check query for URL and prompt navigation
-# print first 50 Google Search result URLs
+# check robots.txt for specs
+# add some random sleep, clicks, movement, actions
+# rotate IPs, proxies, user agents, use headless browser
+# avoid honey pots and logins
+# add depth option for recursive search
+# test TokenBot and potentially fuse approaches
 # make priority queue with value function using content similarity to corpus
-# use top 20% of url's content to 
+# use gradient extraction coefficient (default: linear) to create dataset to train TransformerXL, default is 100% of layer 0, top 90% of layer 1, top 80% of layer 2, etc. to layer 9
+# use exponential gradient
+##
 
-#pip install bs4, requests, scholarly, html5lib, google, progressbar2, clint, etc.
-import sys, os, time, progressbar
-import re #URL Validation
-import bs4 #web scraper
+#pip install bs4, requests, scholarly, html5lib, google, progressbar2, clint, selectolax, warc, spacy, nltk, etc.
+import sys, os, progressbar, time #utilities
+import re #Regex URL Validation
+import shutil, tempfile #copying files
+#import warc #file format for storing web crawls as sequences of content blocks
 import argparse #command line arguments
 import requests #HTTP request
 import scholarly #Google Scholar search
 import webbrowser #open URL in browser
+
 from googlesearch import search #Google Search results list
 from clint.textui import progress #download progress bar
+from io import BytesIO 
+#below doesn't work
+import TokenBot # import * #HTML Parsers, clean-up, and tokenizers
 
-#receive query from command line
+"""METHODS"""
+
+#URL Validation
+def URLV(regex, qry, dn, sn): #URL regex, search query, depth num, and search num
+    if qry is None:
+        print("Empty query. Exiting...")
+        exit()
+    elif re.match(regex, qry):
+        print("Search query: ", query)
+        q = input("Your search query is a website, do you want to go straight to the URL?? Enter yes/no: ")
+        if re.match(q, "yes"):
+            webbrowser.open(qry)
+        elif re.match(q, "no"):
+            results = search_query(qry, sn)
+        else:
+            URLV(regex, qry)
+    else:
+        print("Search query: ", query)
+        results = search_query(qry, sn)
+
+#returns googlesearch generator        
+def search_query(qry, resnum):
+    print("Searching...")
+    #variable number of search results
+    link_gen = search(qry, num=resnum, pause=2)
+
+#gets layer 0 URLs, saves them to storepath, extracts their links and content, and returns both as lists
+def get_search_results(results, storepath, tok_list):
+    URLcount = 0
+    URL_list = []
+    with open(storepath,"w") as sh:
+        sh.write("Query: " + query + "\n\n")
+        for link in results: #Search gen 
+            time.sleep(0.2)  #is this really necessary?? I'll leave it for now.
+            URL_list.append(link)
+            sh.write(link + "\n") #writing layer 0 URLs
+            sh.flush()
+            extract_content_from_URL(link, tok_list)
+            URLcount += 1 
+    return URL_list           
+
+#ensures the response has a Content-Length header
+def ensure_content_length(
+    url, *args, method='GET', session=None, max_size=2**20,  # 1Mb
+    **kwargs):
+    kwargs['stream'] = True
+    session = session or requests.Session()
+    r = session.request(method, url, *args, **kwargs)
+    if 'Content-Length' not in r.headers:
+        # stream content into a temporary file so we can get the real size
+        spool = tempfile.SpooledTemporaryFile(max_size)
+        shutil.copyfileobj(r.raw, spool)
+        r.headers['Content-Length'] = str(spool.tell())
+        spool.seek(0)
+        # replace the original socket with our temporary file
+        r.raw._fp.close()
+        r.raw._fp = spool
+    return r
+
+#extracts and returns list of sentences from URL link and list of tokenizer functions
+def extract_content_from_URL(link, tok_list):
+    r = ensure_content_length(link) #ensures the response has a Content-Length header 
+    contentLen = int(r.headers["Content-Length"]) #not len(r.raw.read())
+    for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(contentLen/1024) + 1): 
+        if chunk:
+            with open(wcontentpath,"w") as cp: #using bs4
+                #cp.write(chunk)
+                txt = get_text_bs(r.content)
+                    
+                    
+            with open(bcontentpath,"w") as cp2: #using selectolax
+                #cp.write(chunk)
+                txt = get_text_selectolax(r.content)
+                if txt:
+                    for s in clean_text(txt):
+                        if not s.isspace() and not s in seensent:
+                            cp2.write(txt)
+                            cp2.flush()
+                            seensent.add(s)
+
+def read_data(storepath, wlcontentpath, blcontentpath):      
+    with open(storepath, "r") as hreader:
+        for line in hreader:
+            print(line)
+
+    with open(wcontentpath, "r") as wlcreader:
+        for line in wlcreader:
+            print(line)
+
+    with open(bcontentpath, "r") as blcreader:
+        for line in blcreader:
+            print(line)
+
+def extract_content(path, txt):
+    sentences = []
+    seensent = set() #set of seen sentences to avoid duplicates
+    if txt:
+        text = ""
+        for s in TokenBot.clean_text(txt): #should I strip this and append everything to one string with spaces? what about paragraphs?
+            text += s #compile all text at URL after preprocessing
+        paras = [para for para in text.strip().splitlines() if para] #should separate all text into paragraphs
+        for p in paras: #for paragraph in paragraphs
+            tokcount = 0
+            for tok in tok_list:
+                for sent in TokenBot.tok(p):
+                    if not (sent in seensent):
+                        placeH = ("Using extraction method #%d: " % tokcount) #displaying which tokenizer
+                        path.write(placeH + sent) #write all sentences in paragraph for each tokenizer
+                        path.flush()
+                        seensent.add(sent)
+                tokcount += 1
+    return sentences            
+
+"""FREE CODE"""
+
+#initializing variables
+cdir = os.getcwd()
+storepath = os.path.join(cdir, "links.txt") #search.txt file in working directory
+wcontentpath = os.path.join(cdir, "wcontent.txt") #content.txt file in working dir
+bcontentpath = os.path.join(cdir, "bcontent.txt") #content2.txt file in working dir
+tok_list = []
+
+#receive query from command line, e.g. os.system("python3 SearchBot.py quicksort -r 5 -n 15 &")
 psr = argparse.ArgumentParser()
 psr.add_argument("q", type=str, help="search query", metavar="query", nargs="*")
-psr.add_argument("-r", dest="refinement", type=int, help="number of results per page, refinement factor", metavar="refinement")
+psr.add_argument("-d", type=int, help="depth of search", metavar="depth")
 psr.add_argument("-n", type=int, help="number of results to search", metavar="number")
 args = psr.parse_args()
-query = str(args.q)
-rnum = 15
-snum = 1000
-if args.refinement is not None:
-    rnum = int(args.refinement)
-if args.n is not None:
-    snum = int(args.n)
+query = None
+depth = 10
+rnum = 1000
+if args.q:
+    query = str(args.q) #don't think I need str, or ints here
+if args.d:
+    depth = int(args.d)
+if args.n:
+    rnum = int(args.n)
 
 #URL Validation Regular Expression
 URLregex = re.compile(
@@ -41,60 +175,6 @@ URLregex = re.compile(
         r'(?::\d+)?' # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-print("\nYour search query is a website:", re.match(URLregex, query) is not None)
-#Direct URL Prompt
-def URLQ(regex, qry):
-    if re.match(regex, qry) is not None:
-        q = input("Do you want to go straight to the URL?? Enter yes/no: ")
-        if re.match(q, "yes"):
-            webbrowser.open(qry)
-        elif re.match(q, "no"):
-            pass
-        else:
-            URLQ(regex, qry)
-URLQ(URLregex, query)
-
-#variable number of search results and results per page
-results = search(query, num=rnum, pause=1.77, stop=snum)
-
-count = 0
-storepath = "C:/Users/vivim/OneDrive/Desktop/MiscCode/search.txt"
-contentpath = "C:/Users/vivim/OneDrive/Desktop/MiscCode/content.txt"
-"""
-if os.path.isdir(storepath) is False:
-os.mkdir(storepath)
-"""
-with open(storepath,"wb") as sh:
-    for link in results:
-        time.sleep(0.2)
-        ''' supposed to be a stable progress bar
-        if snum is not None:
-            print('Search progress: [%d%%]\r'%(count/snum*100))
-            #print('Search progress: [%d%%]\r'%(count/snum*100))
-        else:
-            print('Search progress: [%d%%]\r'%(count/10))
-            #print('Search progress: [%d%%]\r'%(count/10))
-        '''    
-        sh.writelines(link)
-        #print(link)
-        sh.flush()
-        r = requests.get(URL, stream=True)
-        total_length = int(r.headers.get('content-length'))
-        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
-            if chunk:
-                with open(contentpath,"wb") as cp:
-                    cp.write(chunk)
-                    cp.write(r.content)
-                    cp.flush()
-                    
-        count += 1
-        
-with open(storepath, "r") as hreader:
-    for line in hreader:
-        print(line)
-
-with open(contentpath, "r") as creader:
-    for line in creader:
-        print(line)
-        
-os.system("python3 SearchBot.py coding -r 10 -n 100")
+print("Current working directory: ", cdir,  "\n")
+URLV(URLregex, query, rnum)
+print("\nDone.")
--- TokenBot.py
+++ TokenBot.py
@@ -1,17 +1,95 @@
 ## TokenBot
 #
+# extracts text from HTML, and cleans it 
 # tokenizes string input into sentences/words
-#
+# TODO:
+# look up list of all html tags
+# test HTML filtering/list of links scraped
 ##
 
-import re, spacy
-from nltk import tokenize
-from pprint import pprint
+import re #regex
+import spacy, en_core_web_sm #tokenizer & model
+from nltk import tokenize #standard tokenize lib
+#import nltk; nltk.download('punkt')
+from pprint import pprint #no idea
+from bs4 import BeautifulSoup #text parser
+import pyximport; pyximport.install(pyimport=True)
+import parser #faster HTML text parser
+from SearchBot import *
 
-test1="When a child asks her mother \"Where do babies come from?\", what should one reply to her?"
-test2 = "Mr. James told me that Dr. Brown is not available today. I will try again tomorrow."
+#parses HTML using whitelisting to return text and list of URLs
+def get_text_bs(html):
+    tree = BeautifulSoup(html, 'html.parser')
+    text = tree.find_all(text=True)
+    
+    body = tree.body
+    if body is None:
+        return None
 
-testtext = test1 + test2
+    output = ''
+    link_list = []
+
+    whitelist = [
+        'p',
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+        'ul', 'ol', 'li',
+        'div',
+        'table',
+        'a'
+    ]
+
+    for t in text:
+	    if t.parent.name in whitelist:
+                if t.name == 'a':
+                    link_list.append(t.attr('href'))
+                    output += '{} '.format("[URL]")
+                else:
+                    output += '{} '.format(t)    
+    
+    return output, link_list
+
+#parses HTML using blacklisting to return text and list of URLs
+def get_text_selectolax(html):
+    tree = parser.HTMLParser(html)
+
+    if tree.body is None:
+        return None
+
+    link_list = []    
+
+    for url in tree.css('a'):
+        link_list.append(url.attrs["href"])
+        url.text = "[URL]"
+        
+    blacklist = [
+        'link',
+        'nav',
+        'script',
+        'style',
+        'header',
+        'meta',
+        'input',
+        '[document]',
+        'noscript',
+        'head',
+        'html',
+        'footer'
+    ]    
+
+    for sel in blacklist:
+        for tag in tree.css(sel):
+            tag.decompose()    
+   
+    text = tree.body.text(separator=' ') #is this separator ideal??
+    return text, link_list
+
+def clean_text(src):
+    #src = src.replace("\n", " ") #could interfere with paragraph parsing
+    src = src.replace("[\\^]", " ")
+    src = src.replace("\"", " ")
+    src = src.replace("\\s+", " ")
+    #src = src.strip() #redundant
+    return src
 
 alphabets= "([A-Za-z])"
 prefixes = "(Mr|St|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|Mt|Pres|Sr)[.]"
@@ -21,6 +99,22 @@ acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
 websites = "[.](com|net|org|io|gov|me|edu|us|tech|info)"
 digits = "([0-9])"
 
+    
+    
+#python -m spacy download en_core_web_sm
+#word-level
+def extract_words(text):
+    nlp = en_core_web_sm.load()
+    return nlp(text)
+
+#word-level, keeping code/charts together better
+def extract_words2(text):
+    toknz = tokenize.api.StringTokenizer()
+    toknz._string = " "
+    #tokenize_sents() is character-level, w/ span it becomes coordinates
+    return toknz.tokenize(text)
+
+#pretty good
 def extract_sentences1(text):
     text = " " + text + "  "
     text = text.replace("\n"," ")
@@ -39,9 +133,9 @@ def extract_sentences1(text):
     if "\"" in text: text = text.replace(".\"","\".")
     if "!" in text: text = text.replace("!\"","\"!")
     if "?" in text: text = text.replace("?\"","\"?")
-    text = text.replace(".",".<stop>")
-    text = text.replace("?","?<stop>")
-    text = text.replace("!","!<stop>")
+    text = text.replace(". ",".<stop>")
+    text = text.replace("? ","?<stop>")
+    text = text.replace("! ","!<stop>")
     text = text.replace("<prd>",".") #should this go here or later?
     text = text.replace("e.g.","e<prd>g<prd>") #for example
     text = text.replace("i.e.","i<prd>e<prd>") #in other words
@@ -50,46 +144,102 @@ def extract_sentences1(text):
     sentences = sentences[:-1]
     sentences = [s.strip() for s in sentences]
     return sentences
-    
-#python -m spacy download en_core_web_sm
+
+#very good, especially with charts/code    
 def extract_sentences2(text):
-    nlp = spacy.load("en_core_web_sm")
-    return nlp(text)
-    
-def extract_sentences3(text):
     return tokenize.sent_tokenize(text)
 
-def extract_sentences3ish(text):
-    return tokenize.tokenize(text)
-
+#nearly perfect
 def extract_sentences_smart(text):
     trainer = tokenize.punkt.PunktTrainer()
     trainer.INCLUDE_ALL_COLLOCS = True
     trainer.train(text)
 
-    tokz = tokenizer.punkt.PunktSentenceTokenizer(trainer.get_params())
+    tokz = tokenize.PunktSentenceTokenizer(trainer.get_params())
 
+    abbrvs = ['st', 'mrs', 'ms', 'dr', 'prof', 'capt', 'cpt', 'lt',
+     'mt', 'pres', 'sr', 'inc', 'ltd', 'jr', 'sr', 'co']    
+    for abb in abbrvs:
+        tokz._params.abbrev_types.add(abb)   
     print(tokz._params.abbrev_types)
-    tokz._params.abbrev_types.add('dr')
-
-    for decision in tokenizer.debug_decisions(sentences):
+    """
+    for decision in tokz.debug_decisions(text):
         pprint(decision)
         print('=' * 30)
-
+    """
     return tokz.tokenize(text)
 
-def test_tokenizers(text):
-    sentences = extract_sentences1(text)
-    for line in sentences:
-        print(line)
-    sentences = extract_sentences2(text)
-    for line in sentences:
-        print(line)            
-    sentences = extract_sentences3(text)
-    for line in sentences:
-        print(line)    
-    sentences = extract_sentences3ish(text)
-    for line in sentences:
-        print(line)            
 
-test_tokenizers(testtext)        
+test1 = "When a child asks her mother \"Where do babies come from?\", what should one reply to her?"
+test2 = "Mr. James told me that Dr. Brown is not available today. I will try again tomorrow."
+test3 = """To load a model, use spacy.load() with the model name, a shortcut link or a path to the model data directory.
+
+import spacy
+nlp = spacy.load(\"en_core_web_sm\")
+doc = nlp(u\"This is a sentence.\")
+You can also import a model directly via its full name and then call its load() method with no arguments. This should also work for older models in previous versions of spaCy.
+
+import spacy
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
+doc = nlp(u\"This is a sentence.\")
+Manual download and installation
+In some cases, you might prefer downloading the data manually, for example to place it into a custom directory. You can download the model via your browser from the latest releases, or configure your own download script using the URL of the archive file. The archive consists of a model directory that contains another directory with the model data.
+
+└── en_core_web_sm-2.0.0.tar.gz       # downloaded archive
+    ├── meta.json                     # model meta data
+    ├── setup.py                      # setup file for pip installation
+    └── en_core_web_md                # model directory
+        ├── __init__.py               # init for pip installation
+        ├── meta.json                 # model meta data
+        └── en_core_web_sm-2.0.0      # model data
+You can place the model data directory anywhere on your local file system. To use it with spaCy, simply assign it a name by creating a shortcut link for the data directory.
+
+book For more info and examples, check out the models documentation.
+
+spaCy v1.x Releases
+Date	Model	Version	Dep	Ent	Vec	Size	License		
+2017-06-06	es_core_web_md	1.0.0	X	X	X	377 MB	CC BY-SA		
+2017-04-26	fr_depvec_web_lg	1.0.0	X		X	1.33 GB	CC BY-NC		
+2017-03-21	en_core_web_md	1.2.1	X	X	X	1 GB	CC BY-SA		
+2017-03-21	en_depent_web_md	1.2.1	X	X		328 MB	CC BY-SA		
+2017-03-17	en_core_web_sm	1.2.0	X	X	X	50 MB	CC BY-SA		
+2017-03-17	en_core_web_md	1.2.0	X	X	X	1 GB	CC BY-SA		
+2017-03-17	en_depent_web_md	1.2.0	X	X		328 MB	CC BY-SA		
+2016-05-10	de_core_news_md	1.0.0	X	X	X	645 MB	CC BY-SA		
+2016-03-08	en_vectors_glove_md	1.0.0			X	727 MB	CC BY-SA		
+Issues and bug reports
+To report an issue with a model, please open an issue on the spaCy issue tracker. Please note that no model is perfect. Because models are statistical, their expected behaviour will always include some errors. However, particular errors can indicate deeper issues with the training feature extraction or optimisation code. If you come across patterns in the model\'s performance that seem suspicious, please do file a report."""
+testtext = test4
+testURL = "https://en.wikipedia.org/wiki/Machine_learning"
+
+
+tok_list = []
+numtok = 5
+for i in range(numtok):
+    if (i < 3):
+        tok_list.append("extract_sentences%d" % (i + 1))
+    elif (i > 3):
+        tok_list.append("extract_sentences%d" % i)
+    else:
+        tok_list.append("extract_sentences%dish" % (i)) #blip hard coded b/c nltk may have 2 dumb tokenizers
+
+def test_tokenizers(text, toks):
+    '''
+    for tok in toks[2:]:
+        sents = globals()[tok](text)
+        for sent in sents:
+            print(sent)  
+    '''
+    sents = extract_sentences1(text)
+    for sent in sents:
+        print(sent)
+    sents = extract_sentences2(text)
+    for sent in sents:
+        print(sent)
+    snts = extract_sentences_smart(text)
+    for snt in snts:
+        print(snt)
+
+test_tokenizers(testtext, tok_list)        
